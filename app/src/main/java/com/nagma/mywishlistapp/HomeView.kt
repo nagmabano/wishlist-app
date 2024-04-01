a18e0115bf259fe4b92ff46f1595b17468b5f7de
Nagma Bano <bano.nagma.1@gmail.com>
2024-04-02 01:14:16 +0530

    added floating action button

--- app/src/main/java/com/nagma/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/nagma/mywishlistapp/HomeView.kt
@@ -4,11 +4,20 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Colors
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -17,7 +26,17 @@ fun HomeView() {
     Scaffold(
         topBar = { AppBarView(title = "WishList", {
             Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
-        })}
+        },)},
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 20.dp),
+//                contentColor = Color.White,
+//                backgroundColor = Color.Black,
+                onClick = { /*TODO Add navigation to add screen*/
+                }) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
     ) {
         LazyColumn(modifier = Modifier.fillMaxSize().padding(it)) {
 
