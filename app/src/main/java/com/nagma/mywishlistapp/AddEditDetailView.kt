6880ebca6d9737fa683f58bcf56b355ab3bac26e
Nagma Bano <bano.nagma.1@gmail.com>
2024-04-02 11:29:50 +0530

    setting up scaffold for addeditdetail screen

---
+++ app/src/main/java/com/nagma/mywishlistapp/AddEditDetailView.kt
@@ -0,0 +1,46 @@
+package com.nagma.mywishlistapp
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddEditDetailView(
+    id: Long,
+    viewModel: WishViewModel,
+    navController: NavController
+) {
+    Scaffold(
+        topBar = {
+            AppBarView(title =
+            if (id != 0L) stringResource(R.string.add_wish)
+            else stringResource(R.string.update_wish)
+        )
+        }) {
+        Column(modifier = Modifier
+            .padding(it)
+            .wrapContentSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+            ) {
+            Spacer(modifier = Modifier.height(10.dp))
+
+        }
+
+        
+    }
+
+}
