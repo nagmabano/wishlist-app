7866425d7efa91b2d820b2e7a2adf5813fae7316
Nagma Bano <bano.nagma.1@gmail.com>
2024-04-02 13:03:12 +0530

    set up viewModel and AddEditDetailView

--- app/src/main/java/com/nagma/mywishlistapp/AddEditDetailView.kt
+++ app/src/main/java/com/nagma/mywishlistapp/AddEditDetailView.kt
@@ -1,6 +1,5 @@
 package com.nagma.mywishlistapp
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -16,11 +16,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
@@ -43,6 +44,34 @@ fun AddEditDetailView(
             verticalArrangement = Arrangement.Center
             ) {
             Spacer(modifier = Modifier.height(10.dp))
+            WishTextField(value = viewModel.wishTitleState,
+                onValueChanged = {
+                    viewModel.onWishTitleChanged(it)
+                                 },
+                label = "Title")
+
+            Spacer(modifier = Modifier.height(10.dp))
+            WishTextField(value = viewModel.wishDescriptionState,
+                onValueChanged = {
+                    viewModel.onWishDescriptionChanged(it)
+                },
+                label = "Description")
+
+            Button(onClick = {
+                if (viewModel.wishTitleState.isNotEmpty() && viewModel.wishDescriptionState.isNotEmpty()) {
+                    // Update Wish TODO
+                } else {
+                    // TODO Add Wish
+                }
+            }) {
+                Text(
+                    text = if (id != 0L) stringResource(id = R.string.update_wish)
+                    else stringResource(id = R.string.add_wish),
+                    style = TextStyle(
+                        fontSize = 18.sp
+                    )
+                )
+            }
 
         }
 
