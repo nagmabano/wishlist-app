e787c60140eea16dc38332558f94d20df4ad5626
Nagma Bano <bano.nagma.1@gmail.com>
2024-04-03 15:22:48 +0530

    preparing viewModel and using dispatcher & lateinit

--- app/src/main/java/com/nagma/mywishlistapp/WishViewModel.kt
+++ app/src/main/java/com/nagma/mywishlistapp/WishViewModel.kt
@@ -4,8 +4,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nagma.mywishlistapp.data.Wish
+import com.nagma.mywishlistapp.data.WishRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
-class WishViewModel: ViewModel() {
+class WishViewModel(
+    private val wishRepository: WishRepository
+): ViewModel() {
 
     var wishTitleState by mutableStateOf("")
     var wishDescriptionState by mutableStateOf("")
@@ -17,4 +25,37 @@ class WishViewModel: ViewModel() {
     fun onWishDescriptionChanged(newString: String) {
         wishDescriptionState = newString
     }
+
+
+    lateinit var getAllWishes: Flow<List<Wish>>
+
+    init {
+        viewModelScope.launch {
+            getAllWishes = wishRepository.getWishes()
+        }
+    }
+
+    fun addAWish(wish: Wish) {
+        viewModelScope.launch(Dispatchers.IO) {
+            wishRepository.addAWish(wish = wish)
+        }
+    }
+
+    fun getAWishById(id:Long): Flow<Wish> {
+        return wishRepository.getAWishById(id)
+    }
+
+    fun updateAWish(wish: Wish) {
+        viewModelScope.launch(Dispatchers.IO) {
+            wishRepository.updateAWish(wish = wish)
+        }
+    }
+
+    fun deleteWish(wish: Wish) {
+        viewModelScope.launch(Dispatchers.IO) {
+            wishRepository.deleteAWish(wish = wish)
+        }
+    }
+
+
 }
